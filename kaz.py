3c6eae7bcab0afd8279e4973058969446cf639ae
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2023-05-20 16:21:54 +0100

    Base Code

---
+++ kaz.py
@@ -0,0 +1,31 @@
+import pygame
+import knights_archers_zombies
+
+env = knights_archers_zombies.env(render_mode="human",
+    spawn_rate=20,
+    num_archers=1,
+    num_knights=1,
+    max_zombies=10,
+    max_arrows=10,
+    killable_knights=True,
+    killable_archers=True,
+    pad_observation=False,
+    line_death=False,
+    max_cycles=900,
+    vector_state=True,
+    use_typemasks=False)
+env.reset()
+
+clock = pygame.time.Clock()
+
+for agent in env.agent_iter():
+    clock.tick(env.metadata["render_fps"])
+
+    observation, reward, termination, truncation, info = env.last()
+   
+    action = env.action_space(agent).sample()
+
+    if termination:
+        env.step(None)
+    else:
+        env.step(action)
