5093d91b63a3ee1dee51aac5a2a9ac361129621c
iamfatima <marifati.camgon@gmail.com>
2023-05-30 16:19:29 +0100

    Archer now positions itself to the right

--- greedy.py
+++ greedy.py
@@ -65,16 +65,28 @@ class GreedyPolicy:
     def vectors_near_collinear(self, v1, v2):
         #True if vectors are within a margin
         return abs(v1[0] - v2[0]) <= const.ANGLE_MARGIN and abs(v1[1] - v2[1]) <= const.ANGLE_MARGIN
+    
+    def move_agent_to_right(self, v1, v2):
+        return abs(v1[1] - v2[0]) <= const.ARCHER_POSTION_RADIUS and abs(v1[2] - v2[1]) <= const.ARCHER_POSTION_RADIUS
 
-    def archerAction(self,position, target):
+    def archerAction(self, position, target, agent_id):
 
         archer_direction_v = self.unit_vector(np.array([position[3], position[4]]))
         zombie_relational_v = self.unit_vector(np.array([target[1], target[2]]))
-
+        right_vector = self.unit_vector(np.array([1, 0]))
+        right_position_goal = [0.7, 0.825]
         #Archer can have better accuracy if he aims for a zombie position slightly lower than current position:
         #Lower y on game screen => > y_value
         projected_zombie_v = self.unit_vector(np.array([zombie_relational_v[0], zombie_relational_v[1]+const.ARCHER_TARGET_OFFSET]))
-
+        
+        ## Position 2nd archer to the right
+        if int(agent_id) == 1:
+            if not self.move_agent_to_right(position, right_position_goal):
+                if not self.vectors_near_collinear(archer_direction_v, right_vector):
+                    return 3
+                else:
+                    return 0
+                
         #The nearer the zombie is to the archer, the safer it is to use non-projected direction
         chosen_vector = None
         if target[2] >= const.ARCHER_TARGET_CLOSE:
@@ -134,13 +146,14 @@ class GreedyPolicy:
         # [ 0  x  y  rotX  rotY ]
         position = observation[0]
 
+        agent_id = agent[7:]
         if ("archer" in agent):
             # [ AbsDist  RelX  RelY  rotX  rotY ]
             target = self.zombieNearBottom(observation)
             if not target.any():
                 action = 5
             else:
-                action = self.archerAction(position, target)
+                action = self.archerAction(position, target, agent_id)
 
         elif ("knight" in agent):
             # [ AbsDist  RelX  RelY  rotX  rotY ]
