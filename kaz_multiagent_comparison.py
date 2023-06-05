f5f3e9f6cedc607a2034c69a6177cfdc123a4cda
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2023-06-05 16:25:28 +0100

    GRAFICOOOOS

--- kaz_multiagent_comparison.py
+++ kaz_multiagent_comparison.py
@@ -16,132 +16,126 @@ from src import constants as const
 
 N_CASES=len(const.STRATEGY_LIST)
 N_EPISODES=3
+N_GRAPHS=3
 OUTPUT_FILES=[const.STRATEGY_LIST[0]+".txt", const.STRATEGY_LIST[1]+".txt", const.STRATEGY_LIST[2]+".txt"]
 
-METRICS=["Frames", "Total Kills", "Archer Kills", "Knight Kills", "Agent_Deaths"]
-COLORS=["purple", "red", "green", "blue", "yellow"]
+COLORS=["purple", "red", "green", "blue", "lightsalmon", "palegreen", "powderblue"]
 
 def plot_metrics_graph(results):
-    """Creates a bar plot for comparing different agents/teams.
 
-    Parameters
-    ----------
-
-    names: Sequence[str]
-        A sequence of names (representing either the agent names or the team names)
-    means: Sequence[float]
-        A sequence of means (one mean for each name)
-    std_devs: Sequence[float]
-        A sequence of standard deviations (one for each name)
-    N: Sequence[int]
-        A sequence of sample sizes (one for each name)
-    title: str
-        The title of the plot
-    x_label: str
-        The label for the x-axis (e.g. "Agents" or "Teams")
-    y_label: str
-        The label for the y-axis
-    confidence: float
-        The confidence level for the confidence interval
-    show: bool
-        Whether to show the plot
-    filename: str
-        If given, saves the plot to a file
-    colors: Optional[Sequence[str]]
-        A sequence of colors (one for each name)
-    yscale: str
-        The scale for the y-axis (default: linear)
-    """
-
-    # X = ['Group A','Group B','Group C','Group D']
-    # Ygirls = [10,20,20,40]
-    # Zboys = [20,30,25,30]
-
-    names = list(results.keys())
+    # Names of the cases being studied -> X Axis values
+    names = [name[:len(name)-4] for name in list(results.keys())]
     X_axis = np.arange(len(names))
 
-    frames_means = []
-    
-    # Result = [frames, total_kills, archer_kills, knight_kills, agent_deaths] of a case
-    for result in results.values():
-        caseResults = list(results.values())
-        means = [metric.mean() for metric in caseResults[0]]
-        stds = [metric.std() for metric in caseResults[0]]
-        N = [metric.size for metric in caseResults[0]]
+    # Plot 3 bar graphs in  the same row
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 15))
 
-        frames_means += [means[0]]
+    # Calculate means, std_devs and N_samples for all the metrics in all the cases
+    results_means = [[result.mean() for result in list] for list in list(results.values())]
+    results_stds = [[result.std() for result in list] for list in list(results.values())]
+    results_N = [[result.size for result in list] for list in list(results.values())]
 
-    i = 0
-    for metric in METRICS:
-
+    # Compute 3 graphs
+    for idx in range(N_GRAPHS):
         means = []
         stds = []
         N = []
 
-        for result in list(results.values()):
-            means += [result[i].mean()]
-            stds += [result[i].std()]
-            N += [result[i].size]
+        # Graph 1:  'Frames till Game Over' graph
+        if idx == 0:
 
-        i += 1
+            # Frames = idx 0 of results 
+            for case in results_means:
+                means += [case[0]]
+            for case in results_stds:
+                stds += [case[0]]
+            for case in results_N:
+                N += [case[0]]
 
-        plt.bar(X_axis + i, means, 0.4, label = metric)
+            errors = [graph_utils.standard_error(stds[i], N[i], 0.95) for i in range(len(means))]
+            axs[0].bar(X_axis, means, yerr=errors, align='center', alpha=0.5, color=COLORS[0], ecolor='black', capsize=10)
+            axs[0].set_ylabel("Frames", fontsize=12)
+            axs[0].set_xlabel("Test Cases", fontsize=10)
+            axs[0].set_xticks(X_axis)
+            axs[0].set_xticklabels(names)
+            axs[0].yaxis.grid(True)
+            axs[0].set_title("Frames till Game Over", fontsize=14)
+
+        # Graph 2: 'Agent Kills' graph
+        if idx == 1:
+
+            # Total/Archer/Knight Kills = idx 1/2/3
+            for i in [1,2,3]:
+
+                # Reset for each bar
+                means.clear()
+                stds.clear()
+                N.clear()
+
+                for case in results_means:
+                    means += [case[i]]
+                for case in results_stds:
+                    stds += [case[i]]
+                for case in results_N:
+                    N += [case[i]]
+
+                # To help arrange the 3 bars
+                offsets = [-0.2, 0, 0.2] # 3 side to side bars with width=0.2
+                labels = ["Total", "Archers", "Knights"]
+
+                errors = [graph_utils.standard_error(stds[j], N[j], 0.95) for j in range(len(means))]
+                axs[1].bar(X_axis+offsets[i-1], means, width=0.2, yerr=errors, align='center',
+                           alpha=0.5, color=COLORS[i], ecolor='black', capsize=4, label = labels[i-1])
+
+            axs[1].set_ylabel("Kill Count", fontsize=12)
+            axs[1].set_xlabel("Test Cases", fontsize=10)
+            axs[1].set_xticks(X_axis)
+            axs[1].set_xticklabels(names)
+            axs[1].yaxis.grid(True)
+            axs[1].legend()
+            axs[1].set_title("Number of Zombies Killed", fontsize=14)
+
+        # Graph 3: 'Agent Deaths' graph
+        if idx == 2:
+
+            # Total/Archer/Knight Deaths = idx 4/5/6
+            for i in [4,5,6]:
+
+                # Reset for each bar
+                means.clear()
+                stds.clear()
+                N.clear()
+
+                for case in results_means:
+                    means += [case[i]]
+                for case in results_stds:
+                    stds += [case[i]]
+                for case in results_N:
+                    N += [case[i]]
+
+                # To help arrange the 3 bars
+                offsets = [-0.2, 0, 0.2] # 3 side to side bars with width=0.2
+                labels = ["Total", "Archers", "Knights"]
+
+                errors = [graph_utils.standard_error(stds[j], N[j], 0.95) for j in range(len(means))]
+                axs[2].bar(X_axis+offsets[i-4], means, width=0.2, yerr=errors, align='center',
+                        alpha=1, color=COLORS[i], ecolor='black', capsize=4, label = labels[i-4])
+
+            axs[2].set_ylabel("Death Count", fontsize=12)
+            axs[2].set_xlabel("Test Cases", fontsize=10)
+            axs[2].set_xticks(X_axis)
+            axs[2].set_xticklabels(names)
+            axs[2].yaxis.grid(True)
+            axs[2].legend()
+            axs[2].set_title("Number of Dead Agents", fontsize=14)
     
-    
-    # means = [result.mean() for result in results.values()]
-    # stds = [result.std() for result in results.values()]
-    # N = [result.size for result in results.values()]
-    # plot_confidence_bar(
-    #     names=names,
-    #     means=means,
-    #     std_devs=stds,
-    #     N=N,
-    #     title=title,
-    #     x_label="", y_label=f"Avg. {metric}",
-    #     confidence=confidence, show=True, colors=colors
-    # )
-    
-    # plt.bar(X_axis - 0.2, frames_means, 0.4, label = 'Girls')
-    # plt.bar(X_axis + 0.2, Zboys, 0.4, label = 'Boys')
-    
-    plt.xticks(X_axis, names)
-    plt.xlabel("Groups")
-    plt.ylabel("Number of Students")
-    plt.title("Number of Students in each group")
-    plt.legend()
     plt.show()
-
-    # yscale = None
-    # filename = None
-    # show = True
-    # x_label = ""
-    # y_label = "Avg"
-    # title = "Hahayes"
-    # errors = [graph_utils.standard_error(stds[i], N[i], 0.95) for i in range(len(means))]
-    # print(errors)
-    # fig, ax = plt.subplots()
-    # x_pos = np.arange(len(names))
-    # ax.bar(x_pos, means, yerr=errors, align='center', alpha=0.5, color=COLORS if COLORS is not None else "gray", ecolor='black', capsize=10)
-    # ax.bar(x_pos, means, yerr=errors, align='center', alpha=0.5, color="gray", ecolor='black', capsize=10)
-    # ax.set_ylabel(y_label)
-    # ax.set_xlabel(x_label)
-    # ax.set_xticks(x_pos)
-    # ax.set_xticklabels(names)
-    # ax.set_title(title)
-    # ax.yaxis.grid(True)
-    # if yscale is not None:
-    #     plt.yscale(yscale)
-    # plt.tight_layout()
-    # if filename is not None:
-    #     plt.savefig(filename)
-    # if show:
-    #     plt.show()
-    # plt.close()
+    plt.close()
 
 # Delete result files from previous executions
-# for name in OUTPUT_FILES:
-#     if os.path.exists(name):
-#         os.remove(name)
+for name in OUTPUT_FILES:
+    if os.path.exists(name):
+        os.remove(name)
 
 # Create an environment for each test case
 # This is so their output goes to different files
@@ -162,9 +156,7 @@ for i in range(N_CASES):
 
 clock = pygame.time.Clock()
 
-strategy_index = 0
-for env in envs: 
-    break;
+for env, strategy_idx in enumerate(envs): 
     for i in range(N_EPISODES):
         # Reset environment for new episode
         env.reset()
@@ -173,7 +165,7 @@ for env in envs:
 
             observation, reward, termination, truncation, info = env.last()
             
-            greedyPolicy = greedy.GreedyPolicy(env, strategy_index)
+            greedyPolicy = greedy.GreedyPolicy(env, strategy_idx)
 
             #action = env.action_space(agent).sample()
             action = greedyPolicy(observation,agent) #!!! -> esta linha + ficheiro greedy
@@ -182,8 +174,6 @@ for env in envs:
                 env.step(None)
             else:
                 env.step(action)
-
-    strategy_index += 1
 
 # Results is a dictionary: key=case, value=results being plotted
 results = {}
@@ -195,6 +185,8 @@ for case in OUTPUT_FILES:
     archer_kills = []
     knight_kills = []
     agent_deaths = []
+    archer_deaths = []
+    knight_deaths = []
     for episode in f.readlines():
         resultJSON = json.loads(episode)
         frames += [resultJSON["frames"]]
@@ -202,19 +194,18 @@ for case in OUTPUT_FILES:
         archer_kills += [resultJSON["archer_kills"]]
         knight_kills += [resultJSON["knight_kills"]]
         agent_deaths += [resultJSON["dead_agents"]]
+        archer_deaths += [resultJSON["dead_archers"]]
+        knight_deaths += [resultJSON["dead_knights"]]
 
     frames = np.array(frames)
     total_kills = np.array(total_kills)
     archer_kills = np.array(archer_kills)
     knight_kills = np.array(knight_kills)
     agent_deaths = np.array(agent_deaths)
+    archer_deaths = np.array(archer_deaths)
+    knight_deaths = np.array(knight_deaths)
 
-    results[case] = [frames, total_kills, archer_kills, knight_kills, agent_deaths]
+
+    results[case] = [frames, total_kills, archer_kills, knight_kills, agent_deaths, archer_deaths, knight_deaths]
 
 plot_metrics_graph(results)
-
-# graph_utils.compare_results(
-#     results,
-#     title="Comparion between number of total_kills",
-#     colors=COLORS
-# )
